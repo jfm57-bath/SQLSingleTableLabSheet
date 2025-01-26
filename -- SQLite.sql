1a5c85774c95d59d37ecd66326bd6760475c8273
jfm57-bath <jfm57@bath.ac.uk>
2025-01-26 22:27:51 +0000

    Added SQLite file for Question 1

---
+++ -- SQLite.sql
@@ -0,0 +1,23 @@
+-- SQLite
+-- SELECT name, species, sex FROM pet WHERE species = 'snake' OR species
+-- = 'bird';
+
+
+-- Q1-1. The names of owners and their pet's name for all pets who are female.
+-- SELECT owner, name FROM pet WHERE sex = 'f';
+
+-- Q1-2. The names and birth dates of pets which are dogs.
+-- SELECT name, birth FROM pet WHERE species = 'dog';
+
+-- Q1-3. The names of the owners of birds.
+-- SELECT DISTINCT owner FROM pet WHERE species = 'bird';
+
+
+-- Q1-4. The species of pets who are female
+-- SELECT DISTINCT species FROM pet WHERE sex = 'f';
+
+-- Q1-5. The names and birth dates of pets which are cats or birds.
+-- SELECT name, birth FROM pet WHERE species = 'cat' OR species = 'bird';
+
+-- Q1-6. The names and species of pets which are cats or birds and which are female.
+SELECT name, birth FROM pet WHERE species = 'cat' OR species = 'bird' AND sex = 'f';
